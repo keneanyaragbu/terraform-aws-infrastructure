d3d70ccf11e4d4c7bef50bf0b3f227cd04969a1e
keneanyaragbu <117502802+keneanyaragbu@users.noreply.github.com>
2026-06-19 04:07:06 -0700

    jenkins_awx_deploy inventory vars updated

--- scripts/jenkins_awx_deploy.py
+++ scripts/jenkins_awx_deploy.py
@@ -125,7 +125,7 @@ print(f"Using group_id: {group_id}")
 
 # 5. Update inventory vars
 inventory_vars = f"""ansible_user: ubuntu
-ansible_ssh_common_args: '-o StrictHostKeyChecking=no -o ProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p -q ubuntu@{bastion_ip}"'
+ansible_ssh_common_args: '-o StrictHostKeyChecking=no -J ubuntu@{bastion_ip}'
 """
 
 awx_patch(
